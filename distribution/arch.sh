a4b962402833628a00499ef9d52dcf05a2e41d4f
fusion809 <brentonhorne77@gmail.com>
2016-05-16 17:40:29 +1000

    Updating

--- distribution/arch.sh
+++ distribution/arch.sh
@@ -2,6 +2,12 @@
 if comex komodo; then                       # Check if Komodo Edit is already installed
 
   printf "Komodo Edit is already installed!"
+  version
+  instver=$(komodo --version | sed "s/Komodo Edit //g" | sed "s/ .*//g")
+  
+  if ! [[ $major == $instver ]]; then
+    komaur
+  fi
 
 else
 
--- lib/aur.sh
+++ lib/aur.sh
@@ -2,14 +2,6 @@
 
 . ./lib/test.sh
 
-function komin {
-  cd /tmp/komodo-edit
-  makepkg -si --noconfirm
-  cd .
-}
-
-export -f komin
-
 # Install komodo-edit
 function komaur {
 
@@ -17,31 +9,78 @@ function komaur {
   export AUR=https://aur.archlinux.org/cgit/aur.git/snapshot/
   export GIT=https://aur.archlinux.org/
 
-  if comex yaourt; then                    # Install with yaourt if possible
+  printf "Would you prefer to install Komodo Edit from the AUR or from my PKGBUILDs GitHub repository? [AUR/GitHub/?; GitHub is the default] "
+  read choice
+
+  if [[ $choice == "?" ]]; then
+
+    printf 'If you opt to install Komodo from the AUR, the AUR helper `yaourt` will be used to install it. This means that when updates become available from the AUR you will be able to install them (along with packages updates from the pacman repositories and other AUR packages you have installed) by running `yaourt -Syua`.\nMy PKGBUILD for Komodo Edit should be up-to-date and uses a desktop configuration file with MimeType support for a wider variety of different file formats than that provided by the AUR.'
+
+    printf "Would you prefer to install Komodo Edit from the AUR or from my PKGBUILDs GitHub repository? [AUR/GitHub; GitHub is the default] "
+    read choice
+
+  fi
+
+  if [[ $choice == "AUR" ]]; then
+
+    if comex yaourt; then                    # Install with yaourt if possible
+
+      printf "Great you have yaourt already installed!"
+
+    elif comex git; then                      # Install with git and makepkg otherwise
+
+      printf "Ah, it seems you do not have yaourt installed, so I'm about to install it for you!"
+
+      git clone $GIT/package-query.git /tmp/package-query
+      git clone $GIT/yaourt.git /tmp/yaourt
+
+      cd /tmp/package-query && makepkg -si --noconfirm
+      cd /tmp/yaourt && makepkg -si --noconfirm
+
+    elif comex curl; then                     # Install with curl and makepkg otherwise
+
+      curl -sL $AUR/package-query.tar.gz | tar xz -C /tmp
+      curl -sL $AUR/yaourt.tar.gz | tar xz -C /tmp
+      cd /tmp/package-query && makepkg -si --noconfirm
+      cd /tmp/yaourt && makepkg -si --noconfirm
+
+    elif comex wget; then                     # Install with wget and makepkg otherwise
+
+      wget -cqO- $AUR/package-query.tar.gz | tar xz -C /tmp
+      wget -cqO- $AUR/yaourt.tar.gz | tar xz -C /tmp
+      cd /tmp/package-query && makepkg -si --noconfirm
+      cd /tmp/yaourt && makepkg -si --noconfirm
+
+    else                                      # Install curl and install with curl and makepkg otherwise
+
+      sudo pacman -S curl --noconfirm
+      curl -sL $AUR/package-query.tar.gz | tar xz -C /tmp
+      curl -sL $AUR/yaourt.tar.gz | tar xz -C /tmp
+      cd /tmp/package-query && makepkg -si --noconfirm
+      cd /tmp/yaourt && makepkg -si --noconfirm
+
+    fi
 
     yaourt -S komodo-edit --noconfirm
 
-  elif comex git; then                      # Install with git and makepkg otherwise
+  else # GitHub is the default
 
-    git clone $GIT/komodo-edit.git /tmp/komodo-edit
-    komin
+    if comex git; then
 
-  elif comex curl; then                     # Install with curl and makepkg otherwise
+      git clone https://github.com/fusion809/PKGBUILDs.git /tmp/PKGBUILDs
 
-    curl -sL $AUR/komodo-edit.tar.gz | tar xz -C /tmp
-    komin
+    elif comex curl; then
 
-  elif comex wget; then                     # Install with wget and makepkg otherwise
+      curl -sL https://github.com/fusion809/PKGBUILDs/archive/master.tar.gz | tar xz --transform=s/PKGBUILDs-master/PKGBUILDs -C /tmp
 
-    wget -cqO- $AUR/komodo-edit.tar.gz | tar xz -C /tmp
-    komin
+    elif comex wget; then
 
-  else                                      # Install curl and install with curl and makepkg otherwise
+      wget -cqO- https://github.com/fusion809/PKGBUILDs/archive/master.tar.gz | tar xz --transform=s/PKGBUILDs-master/PKGBUILDs -C /tmp
 
-    sudo pacman -S curl --noconfirm
-    curl -sL $AUR/komodo-edit.tar.gz | tar xz -C /tmp
-    komin
+    fi
 
+    cd /tmp/PKGBUILDs/komodo-edit
+    makepkg -si --noconfirm
   fi
 }
 
