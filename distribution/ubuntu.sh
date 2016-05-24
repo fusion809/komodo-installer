527cdd9a684a84349d9fe656eef6d15d601f561b
fusion809 <brentonhorne77@gmail.com>
2016-05-25 08:40:14 +1000

    Updating

--- distribution/ubuntu.sh
+++ distribution/ubuntu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install -y libgcc1 libgtk2.0-dev libjpeg-turbo-progs dbus-glib
+sudo apt-get install -y libgcc1 libgtk2.0-dev libjpeg-turbo-progs libdbus-glib-1-2
 komodo_install
